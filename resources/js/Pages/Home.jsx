93e47098e655eed64bc2b4b027c029ecf055608f
Wasuremeno <wasuremono19@gmail.com>
2025-09-22 13:54:52 +0300

    seeding, migrations and blade template

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,10 @@
+import ChatLayout from '@/Layouts/ChatLayout';
+import { Head } from '@inertiajs/react';
+
+export default function Home () {
+    return (
+        <ChatLayout>
+            Messages
+        </ChatLayout>
+    );
+}
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,13 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+            server: {
+            host: '192.168.0.100',
+            port: 8000,
+            hmr: {
+                port: 8000,
+            }
+        },
     plugins: [
         laravel({
             input: 'resources/js/app.jsx',
